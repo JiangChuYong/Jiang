f42521fead06b58aefbf7a621a6bd473dd24ae90
HeberJiang <1427517880@qq.com>
2016-02-17 11:09:51 +0800

    null

--- LincKia/HomePageViewController.h
+++ LincKia/HomePageViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface HomePageViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@interface HomePageViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UITabBarControllerDelegate>
 
 
 @end
